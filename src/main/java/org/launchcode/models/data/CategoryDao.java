5af6878e298418b801e1da4cbd4feb56a7099093
jarrett brown <jarrettjbrown@gmail.com>
2017-06-26 14:26:55 -0500

    Part 2 complete - minus bonus

--- src/main/java/org/launchcode/models/data/CategoryDao.java
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -2,9 +2,14 @@ package org.launchcode.models.data;
 
 import org.launchcode.models.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by jarrett on 6/19/2017.
  */
+@Repository
+@Transactional
 public interface CategoryDao extends CrudRepository<Category, Integer> {
 }
